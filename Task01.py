7b9dd81d3a113d31576ec5620d68237564a87781
Dmitriy Ozyabkin <ozyabkin.d@gmail.com>
2024-09-01 20:35:51 +0300

    Task01 completed

--- Task01.py
+++ Task01.py
@@ -1,2 +1,16 @@
 with open('numbers.txt', 'r', encoding='utf-8') as file:
-    
+    nums = file.readlines()
+    num_list = []
+
+    for line in nums:
+        for elem in line.split():
+            try:
+                elem = int(elem)
+                num_list.append(elem)
+            except:
+                continue
+    answer = sum(num_list)
+    print(answer)
+
+with open('answer.txt', 'w', encoding='utf-8') as file:
+    file.write(str(answer))
